d99dabebde6439b4791cb0ed06a50c9ddc897de0
Clark Rasmussen <crasmussen@inkling.com>
2017-05-09 12:00:53 -0700

    Move test folder

---
+++ test/once.test.js
@@ -0,0 +1,30 @@
+import * as _ from 'lodash';
+
+/**
+ * Creates a function that is restricted to invoking func once. Repeat calls to the
+ * function return the value of the first invocation. The func is invoked with the
+ * this binding and arguments of the created function.
+ *
+ * @param {Function} func The function to restrict.
+ *
+ * @return {Function} The new restricted function.
+ */
+function onceClone(func) {
+
+}
+
+describe('once', () => {
+    let mock;
+
+    beforeEach(() => {
+        mock = jest.fn();
+    });
+
+    it('calls the function once', () => {
+        const mockOnce = _.once(mock);
+        mockOnce(1);
+        mockOnce(2);
+        expect(mock).toHaveBeenCalledTimes(1);
+        expect(mock).toHaveBeenLastCalledWith(1);
+    });
+});
