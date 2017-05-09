d0a4ee3018f9b9a745a9780d63ab8d066f7d5400
Clark Rasmussen <crasmussen@inkling.com>
2017-05-09 10:48:48 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": ["airbnb", "plugin:jasmine/recommended"],
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
---
+++ src/flatten.test.js
@@ -0,0 +1,30 @@
+import * as _ from 'lodash';
+
+
+/**
+ * Flattens an array a single level deep.
+ *
+ * @param array <Array> The array to flatten.
+ *
+ * @return {Array} A new array.
+ */
+function flattenClone(array) {
+
+}
+
+describe('flatten', () => {
+    it('flattens an already flattened array', () => {
+        const array = _.flatten([1, 2, 3, 4, 5]);
+        expect(array).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    it('flattens a nested array', () => {
+        const array = _.flatten([1, 2, [3, 4], 5]);
+        expect(array).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    it('does not complete flatten a deeply nested array', () => {
+        const array = _.flatten([1, 2, [[3, 4], 5]]);
+        expect(array).toEqual([1, 2, [3, 4], 5]);
+    });
+});
