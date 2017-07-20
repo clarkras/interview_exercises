f8823551287c8d459202049f4ef146ec71e1233b
Clark Rasmussen <crasmussen@inkling.com>
2017-07-20 14:09:27 -0700

    Fix type

--- test/once.test.js
+++ test/once.test.js
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
  * Creates a function that is restricted to invoking a function once. Repeat calls to the
  * function return the value of the first invocation.
  *
- * The func is invoked with the this binding and arguments of the created function.
+ * The function is invoked with the this binding and arguments of the created function.
  *
  * @param {Function} func The function to restrict.
  *
