47203eb1d7bbe72f9d78c78899693fb55e579eda
code <code@inklingcodechallenge.local>
2017-12-11 14:49:09 -0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
-    "extends": ["airbnb", "plugin:jasmine/recommended"],
+    "extends": "airbnb",
     "plugins": [
         "react",
         "jsx-a11y",
         "import"
-    ],
-    "rules": {
-      "jsx-a11y/img-has-alt": "off"
-      "jsx-a11y/label-uses-for": "off",
-      "jsx-a11y/mouse-events-map-to-key-events": "off",
-      "jsx-a11y/no-hash-href": "off",
-      "jsx-a11y/redundant-alt": "off",
-      "jsx-a11y/valid-aria-role": "off"
-
+    ]
 };
