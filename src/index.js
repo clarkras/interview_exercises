68e59a63962078a589a0c95d633c70372089dadc
Clark Rasmussen <crasmussen@inkling.com>
2018-01-19 12:53:59 -0800

    Simplify directory structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import Counter from './components/Counter';
+import Counter from './Counter';
 
 const initialState = {
   count: 0
